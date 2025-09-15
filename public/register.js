983e2f8b2beeb1b4507fa97878e225121f7ed2c6
celicular <mcking090907@gmail.com>
2025-09-15 21:08:16 +0530

    no

--- public/register.js
+++ public/register.js
@@ -2,7 +2,7 @@ regButton = document.getElementById('submit');
 import API_URL from "./config";
 
 function register(data) {
-    fetch(`/register`, {
+    fetch(`${API_URL}/register`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
